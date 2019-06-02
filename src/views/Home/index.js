a7a491b6391611aff976b0e3aaf42c7af875aa01
liyaocool <crazyliyao@163.com>
2019-06-02 12:23:17 +0800

    引入Antd UI 框架

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,15 +1,16 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import { Button } from 'antd';
 import HomeDetail from "./HomeDetail/index";
 
 export default class Home extends Component {
   render() {
     return (
       <div>
-        <Router basename="/Home">
+        <Router basename="/">
           <h1>Home</h1>
           <Link to={{ pathname: "/HomeDetail", params: { test: "aaa" } }}>
-            首页详情页
+            <Button type="primary">首页详情页</Button>
           </Link>
 
           <Switch>
