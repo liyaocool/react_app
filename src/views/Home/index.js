9ec9048c7809ac65c972c00544286d1080acf51e
liyaocool <crazyliyao@163.com>
2019-06-05 23:57:12 +0800

    父子组件传值,调用方法

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,19 +1,48 @@
-import React, { Component } from "react";
-// import { Button } from "antd";
+import React, { useState, useRef } from "react";
 
-export default class Home extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
+//子组件
+function ChildComp(props, ref) {
+  const [city,setCity] = useState('')
+  function childMethod(props) {
+    setCity(city + '--TnT--')
+    console.log("我是子组件方法", city);
+    props.parentMethod(city)
   }
-  componentDidMount() {
-    console.log(this.props);
+  return (
+    <div style={{ backgroundColor: "#ddd" }}>
+      <p>我是子组件</p>
+      <button onClick={()=>childMethod(props)}>
+        子组件按钮
+      </button>
+      <h1>父组件传递的参数:{props.abc}</h1>
+    </div>
+  );
+}
+
+//父组件
+export default function Home(props) {
+  const [count, setCount] = useState(0);
+  const child_test = useRef(null);
+  const parentMethod = (city) => {
+    setCount(count + 1);
+    console.log("我是父组件方法",city);
+  };
+  const toFocus = () => {
+    console.log("child_test", child_test);
+    child_test.current.focus();
+  };
+  const handleChange = () => {
+    setCount( child_test.current.value)
   }
-  render() {
-    return (
-      <div>
-        <h1>首页-页面</h1>
-      </div>
-    );
-  }
+  return (
+    <div style={{ backgroundColor: "#eee" }}>
+      <h1>Home页面</h1>
+      <hr />
+      <p>我是父组件</p>
+      <button onClick={toFocus}>父组件,获取焦点</button>
+      <h1>计数:{count}</h1>
+      <input ref={child_test} value={count} onChange={handleChange} type="text" />
+      <ChildComp abc="123"  parentMethod={parentMethod}/>
+    </div>
+  );
 }
--- src/views/User/index.js
+++ src/views/User/index.js
@@ -1,40 +1,13 @@
-import React, { createRef, useState, useEffect } from "react";
-
-function MyComponent(props) {
-  const myDebug = () => {
-    console.log("我是子组件方法");
-  };
-  return <h1>{props.abc}</h1>;
-}
+import React, { useState, useEffect } from "react";
 
 export default function User(props) {
   const [count, setCount] = useState(0);
 
-  useEffect(() => {
-    startTimer();
-    return () => {
-      clearInterval(timer);
-    };
-  });
-
-  let timer = setInterval(() => {
-    console.log("-----------");
-  }, 2000);
-
-  const startTimer = () => timer;
-  const parentDebug = () => {
-    console.log("我是父组件方法");
-  };
-  function clickChild() {
-    console.log(232)
-  }
   return (
     <div>
-      <MyComponent abc="123"/>
+      <h1>user页面</h1>
+      <hr></hr>
       <h1>{count}</h1>
-      <input onChange={e => setCount(e.target.value)} />
-      <br></br>
-      <button onClick={clickChild}>点击</button>
     </div>
   );
 }
