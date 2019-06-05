121360d3181b1f8de859cb182b0d4718eca8f65f
liyaocool <crazyliyao@163.com>
2019-06-05 18:12:34 +0800

    使用HOOK

--- src/views/User/index.js
+++ src/views/User/index.js
@@ -1,12 +1,40 @@
-import React, { useState } from "react";
+import React, { createRef, useState, useEffect } from "react";
+
+function MyComponent(props) {
+  const myDebug = () => {
+    console.log("我是子组件方法");
+  };
+  return <h1>{props.abc}</h1>;
+}
 
 export default function User(props) {
   const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    startTimer();
+    return () => {
+      clearInterval(timer);
+    };
+  });
+
+  let timer = setInterval(() => {
+    console.log("-----------");
+  }, 2000);
+
+  const startTimer = () => timer;
+  const parentDebug = () => {
+    console.log("我是父组件方法");
+  };
+  function clickChild() {
+    console.log(232)
+  }
   return (
     <div>
-      <text>1{process.env.REACT_APP_ABC}</text>
+      <MyComponent abc="123"/>
       <h1>{count}</h1>
-      <button onClick={() => setCount(count + 1)}>递增</button>
+      <input onChange={e => setCount(e.target.value)} />
+      <br></br>
+      <button onClick={clickChild}>点击</button>
     </div>
   );
 }
