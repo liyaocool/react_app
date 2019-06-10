b677c5f91dc0538e7e6e473c33d2e89daf2864cf
liyaocool <crazyliyao@163.com>
2019-06-10 17:06:13 +0800

    父子组件互相调用

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,48 +1,58 @@
-import React, { useState, useRef } from "react";
+import React, {
+  useState,
+  forwardRef,
+  useRef,
+  useImperativeHandle
+} from "react";
 
 //子组件
 function ChildComp(props, ref) {
-  const [city,setCity] = useState('')
-  function childMethod(props) {
-    setCity(city + '--TnT--')
-    console.log("我是子组件方法", city);
-    props.parentMethod(city)
+  function HaHa() {
+    console.log("哈哈,我是子组件内的方法");
+    props.parentMethod()
   }
-  return (
-    <div style={{ backgroundColor: "#ddd" }}>
-      <p>我是子组件</p>
-      <button onClick={()=>childMethod(props)}>
-        子组件按钮
-      </button>
-      <h1>父组件传递的参数:{props.abc}</h1>
-    </div>
-  );
+  useImperativeHandle(ref, () => ({
+    HaHa
+  }));
+  return <>
+    <h1>子组件</h1>
+    <h1>子组件接收到的参数:{props.num}</h1>
+    <button onClick={HaHa}>子组件按钮</button>
+  </>;
 }
+ChildComp = forwardRef(ChildComp);
 
 //父组件
-export default function Home(props) {
+export default function Home(props, ref) {
   const [count, setCount] = useState(0);
   const child_test = useRef(null);
-  const parentMethod = (city) => {
+  const my_ref = useRef(null);
+  const parentMethod = city => {
     setCount(count + 1);
-    console.log("我是父组件方法",city);
+    console.log("我是父组件方法", city);
   };
   const toFocus = () => {
     console.log("child_test", child_test);
     child_test.current.focus();
+    my_ref.current.HaHa();
   };
   const handleChange = () => {
-    setCount( child_test.current.value)
-  }
+    setCount(child_test.current.value);
+  };
   return (
     <div style={{ backgroundColor: "#eee" }}>
       <h1>Home页面</h1>
       <hr />
       <p>我是父组件</p>
-      <button onClick={toFocus}>父组件,获取焦点</button>
       <h1>计数:{count}</h1>
-      <input ref={child_test} value={count} onChange={handleChange} type="text" />
-      <ChildComp abc="123"  parentMethod={parentMethod}/>
+      <button onClick={toFocus}>父组件按钮</button>
+      <input
+        ref={child_test}
+        value={count}
+        onChange={handleChange}
+        type="text"
+      />
+      <ChildComp ref={my_ref} num={count} parentMethod={parentMethod} />
     </div>
   );
 }
--- src/views/MyPlugin/index.js
+++ src/views/MyPlugin/index.js
@@ -1,20 +1,50 @@
-import React, { Component } from "react";
-
-class MyPlugin extends Component {
+import React, { Component, createRef, forwardRef } from "react";
+import propTypes from "prop-types";
+class Child extends Component {
   constructor(props) {
     super(props);
     this.state = {};
+    this.ChildMethod = this.ChildMethod.bind(this);
   }
-  componentDidMount() {
-    console.log(this.props);
+  ChildMethod() {
+    // this.props.ParentMethod();
+    console.log(`子组件方法`);
   }
+
   render() {
     return (
       <div>
-        <h1>插件-页面</h1>
+        <button onClick={this.ChildMethod}>子组件调用父组件方法</button>
+        Child
       </div>
     );
   }
 }
 
-export default MyPlugin;
+Child.propTypes = {
+  ParentMethod: propTypes.func
+};
+
+export default class MyPlugin extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.ParentMethod = this.ParentMethod.bind(this);
+  }
+  ParentMethod() {
+    // console.log("父组件方法");
+    this.abc.ChildMethod()
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.ParentMethod}>父组件点击</button>
+        MyPlugin
+        <Child
+          ref={ref1 => (this.abc = ref1)}
+          // ParentMethod={this.ParentMethod}
+        />
+      </div>
+    );
+  }
+}
